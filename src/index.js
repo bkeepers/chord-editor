155494f4a78251810b29763caf9e7d54be4450a8
Brandon Keepers <brandon@opensoul.org>
2020-05-23 13:35:18 -0400

    Support multiple input formats

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
 import ChordSheetJS from "@ftes/chordsheetjs";
 import "./style/index.css";
 
-const parser = new ChordSheetJS.UltimateGuitarParser({
-  preserveWhitespace: false
-});
+const parsers = {
+  chordpro: new ChordSheetJS.ChordProParser(),
+  chordsheet: new ChordSheetJS.ChordSheetParser({preserveWhitespace: false}),
+  ultimate: new ChordSheetJS.UltimateGuitarParser({preserveWhitespace: false}),
+}
+
 const formatters = {
+  html: new ChordSheetJS.HtmlTableFormatter(),
   chordpro: new ChordSheetJS.ChordProFormatter(),
   latex: new ChordSheetJS.LatexFormatter()
 };
 
 function convert() {
-  const input = document.getElementById("ultimate").value;
-  const parsed = parser.parse(input);
-  const formatEl = document.getElementById("format");
-  const format = formatEl.options[formatEl.selectedIndex].value;
-  const output = formatters[format].format(parsed);
-  document.getElementById("chordpro").value = output;
+  const input = document.getElementById("input").value;
+  const inputFormatEl = document.getElementById("input-format");
+  const inputFormat = inputFormatEl.options[inputFormatEl.selectedIndex].value;
+
+  console.log({inputFormat})
+  const parsed = parsers[inputFormat].parse(input);
+
+  const outputFormatEl = document.getElementById("output-format");
+  const outputFormat = outputFormatEl.options[outputFormatEl.selectedIndex].value;
+  const output = formatters[outputFormat].format(parsed);
+  var rendered = document.getElementById("output-rendered");
+  var source = document.getElementById("output-source");
+  rendered.hidden = source.hidden = true
+  if(outputFormat == "html") {
+    rendered.hidden = false
+    rendered.innerHTML = output;
+  } else {
+    source.hidden = false
+    source.value = output;
+  }
 }
 
 let toggleState = document.getElementById("toggle").checked;
@@ -30,6 +48,7 @@ function convertIfToggled() {
 
 document.getElementById("convert").addEventListener("click", convert);
 document.getElementById("toggle").addEventListener("change", setToggle);
-document.getElementById("ultimate").addEventListener("keyup", convertIfToggled);
-document.getElementById("format").addEventListener("change", convert);
+document.getElementById("input").addEventListener("keyup", convertIfToggled);
+document.getElementById("input-format").addEventListener("change", convert);
+document.getElementById("output-format").addEventListener("change", convert);
 convert();
